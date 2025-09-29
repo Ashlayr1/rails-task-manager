56ccc8e60b8b88a80c5ff0adad247eac22b1e034
Vikesh Rooplall <rooplallv50@gmail.com>
2025-09-29 22:46:12 +0400

    implemented rails task manager edit

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get '/index', to: 'tasks#index', as: 'index'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  get '/show/:id', to: 'tasks#show', as: 'task'
+  get "tasks/:id/edit", to: "tasks#edit", as: 'edit'
+  patch "tasks/:id", to: "tasks#update", as: 'update'
 end
